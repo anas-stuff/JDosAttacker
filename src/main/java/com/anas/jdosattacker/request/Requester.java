e190e948462033373a06a68012fddeb726a2391b
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-29 11:05:36 +0200

    Replace replaceAll method with replace method

--- src/main/java/com/anas/jdosattacker/request/Requester.java
+++ src/main/java/com/anas/jdosattacker/request/Requester.java
@@ -19,7 +19,7 @@ public class Requester implements Runnable {
         userAgent = """
             Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
              Chrome/60.0.3112.113 Safari/537.36
-             """.replaceAll("\n", "");
+             """.replace("\n", "");
         setRequestMethod(null); // null means GET
     }
 
