e9e15e6c47a726e0fabbdbbed4053c5ae133bcf7
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-07-22 09:28:48 +0200

    Create the java doce for the FieldException class😇

--- src/main/java/com/anas/jdosattacker/FieldException.java
+++ src/main/java/com/anas/jdosattacker/FieldException.java
@@ -1,7 +1,10 @@
 package com.anas.jdosattacker;
 
+/**
+ * `FieldException` is a custom exception class that is thrown when a field value is wrong.
+ */
 public class FieldException extends Exception {
-    public FieldException(String message) {
+    public FieldException(final String message) {
         super(message);
     }
 }
